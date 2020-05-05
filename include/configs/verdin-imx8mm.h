Mubhadiya-eic/u-boot-toradex

commit 099bc1b8ef7994781fc0ad1ed2abdc1dd8d5b6e3

verdin-imx8mm: choose correct devicetree with configblock
This commit makes u-boot choose the right variant wifi or nonwifi
of the devicetree with the information stored in toradex configblock.

Related-to: ELB-2642
Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>

include/configs/verdin-imx8mm.h
@@ -95,6 +95,8 @@
 #define CONFIG_SERVERIP		192.168.10.1
 #endif /* CONFIG_CMD_NET */
 
+#define FDT_FILE "fsl-imx8mm-verdin-${variant}-${fdt_board}.dtb"
+
 #define MEM_LAYOUT_ENV_SETTINGS \
 	"fdt_addr_r=0x44000000\0" \
 	"kernel_addr_r=0x42000000\0" \
@@ -106,6 +108,7 @@
 
 /* Enable Distro Boot */
 #ifndef CONFIG_SPL_BUILD
+#define CONFIG_BOOTCOMMAND "setenv fdtfile " FDT_FILE " && run distro_bootcmd;"
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 1) \
 	func(MMC, mmc, 0) \
@@ -124,7 +127,7 @@
 	"bootcmd_mfg=fastboot 0\0" \
 	"console=ttymxc0\0" \
 	"fdt_addr=0x43000000\0" \
-	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"fdt_board=dev\0" \
 	"initrd_addr=0x43800000\0" \
 	"initrd_high=0xffffffffffffffff\0" \
 	"kernel_image=Image\0" \

board/toradex/verdin-imx8mm/verdin-imx8mm.c
@@ -523,6 +523,17 @@ size_t display_count = ARRAY_SIZE(displays);
 
 int board_late_init(void)
 {
+#ifdef CONFIG_TDX_CFG_BLOCK
+	/*
+	 * If we have a valid config block and it says we are a module with
+	 * Wi-Fi/Bluetooth make sure we use the -wifi device tree.
+	 */
+	if (tdx_hw_tag.prodid == VERDIN_IMX8MMQ_WIFI_BT_IT)
+		env_set("variant", "wifi");
+	else
+		env_set("variant", "nonwifi");
+#endif
+
 	return 0;
 }
 

configs/verdin-imx8mm_defconfig
@@ -11,7 +11,6 @@ CONFIG_SPL_LOAD_FIT=y
 CONFIG_SYS_EXTRA_OPTIONS="IMX_CONFIG=arch/arm/mach-imx/spl_sd.cfg,SPL_TEXT_BASE=0x7E1000"
 CONFIG_BOOTDELAY=1
 CONFIG_LOG=y
-CONFIG_DEFAULT_FDT_FILE="fsl-imx8mm-verdin-wifi-dev.dtb"
 CONFIG_VERSION_VARIABLE=y
 # CONFIG_DISPLAY_BOARDINFO is not set
 CONFIG_ARCH_MISC_INIT=y

commit 77c102fe7fd1c7c495afbb47e9081d38374ded87

verdin-imx8mm: fix CONFIG_BOOTCOMMAND redefinition warnings
As default value for CONFIG_BOOTCOMMAND is already provided in
common/Kconfig, manually redefining without a check in verdin-imx8mm.h
leads to these warnings:

include/configs/verdin-imx8mm.h:111: warning: "CONFIG_BOOTCOMMAND" redefined
 #define CONFIG_BOOTCOMMAND "setenv fdtfile " FDT_FILE " && run distro_bootcmd;"
In file included from ././include/linux/kconfig.h:4,
                 from <command-line>:
include/generated/autoconf.h:203: note: this is the location of the previous definition
 #define CONFIG_BOOTCOMMAND "run distro_bootcmd"

Fix that by undefining the previous value.

Fixes: 099bc1b8ef("verdin-imx8mm: choose correct devicetree with configblock")
Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>

include/configs/verdin-imx8mm.h
@@ -108,6 +108,7 @@
 
 /* Enable Distro Boot */
 #ifndef CONFIG_SPL_BUILD
+#undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND "setenv fdtfile " FDT_FILE " && run distro_bootcmd;"
 #define BOOT_TARGET_DEVICES(func) \
 	func(MMC, mmc, 1) \