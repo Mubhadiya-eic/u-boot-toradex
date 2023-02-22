beagleboard/u-boot

commit 9d9fd0a3b18f4a80df3cb5dbad37dcccab97ed31

misc: uclass: Introduce misc_init_by_ofnode
Introduce misc_init_by_ofnode to probe a misc device
using its ofnode.

Signed-off-by: Keerthy <j-keerthy@ti.com>

include/misc.h
@@ -76,6 +76,15 @@ int misc_call(struct udevice *dev, int msgid, void *tx_msg, int tx_size,
  */
 int misc_set_enabled(struct udevice *dev, bool val);
 
+/**
+ * misc_init_by_ofnode() - Probe a misc device by using ofnode.
+ * @node: ofnode of the misc device.
+ *
+ * A misc device is probed using ofnode.
+ *
+ * Return: -ve on error, 0 on success
+ */
+int misc_init_by_ofnode(ofnode node);
 /*
  * struct misc_ops - Driver model Misc operations
  *

drivers/misc/misc-uclass.c
@@ -5,6 +5,8 @@
 
 #include <common.h>
 #include <dm.h>
+#include <dm/device-internal.h>
+#include <dm/uclass-internal.h>
 #include <errno.h>
 #include <misc.h>
 
@@ -63,6 +65,29 @@ int misc_set_enabled(struct udevice *dev, bool val)
 		return -ENOSYS;
 
 	return ops->set_enabled(dev, val);
+}
+
+int misc_init_by_ofnode(ofnode node)
+{
+	struct udevice *dev = NULL;
+	struct uclass *uc;
+	int ret;
+
+	ret = uclass_get(UCLASS_MISC, &uc);
+	if (ret)
+		return ret;
+
+	uclass_foreach_dev(dev, uc) {
+		if (ofnode_equal(node, dev_ofnode(dev))) {
+			ret = device_probe(dev);
+			if (ret)
+				debug("%s: Failed to initialize - %d\n",
+				      dev->name, ret);
+			return ret;
+		}
+	}
+
+	return -ENODEV;
 }
 
 UCLASS_DRIVER(misc) = {

Mubhadiya-eic/u-boot-toradex

commit 9ae53d1bb4b192041770877c253da255b2b463a2

misc: uclass: Fix compile error
Add #include <dm/ofnode.h> to fix the following compiler error

+In file included from drivers/pinctrl/nxp/pinctrl-scu.c:11:
+include/misc.h:87:25: error: unknown type name 'ofnode'
+   87 | int misc_init_by_ofnode(ofnode node);
+      |                         ^~~~~~

Upstream-Status: Inappropriate [other]

The fixed commit is present only on the downstream TI branch,
therefore there is nothing to send to upstream denx repository.
We might think about sending this patch to TI in the future.

Fixes: 9d9fd0a3b18f ("misc: uclass: Introduce misc_init_by_ofnode")
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

include/misc.h
@@ -6,6 +6,8 @@
 #ifndef _MISC_H_
 #define _MISC_H_
 
+#include <dm/ofnode.h>
+
 /**
  * misc_read() - Read the device to buffer, optional.
  * @dev: the device