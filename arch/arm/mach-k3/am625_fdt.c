b6e669d7a3939f6c1491fa54975caa7f954458d5
Emanuele Ghidoli <emanuele.ghidoli@toradex.com>
2023-07-13 13:12:18 +0200

    arm: k3: Fix ft_system_setup so it can be enabled on any SoC
    ft_system_setup cannot be enabled on SoC without msmc sram otherwise
    fdt_fixup_msmc_ram function fails causing system reset.
    
    Fix by moving fdt_fixup_msmc_ram to common_fdt.c file and creating
    SoC (AM654, J721E and J721S2) specific files for fdt fixups.
    
    This change was verified to not change anything on any existing board
    (all the J721S2, AM654 and J721E boards requires it,
    none of the remaining k3 boards require it).
    
    Upstream-Status: Backport [7b7288df3415c7faa4a305b814bb8932876b2149]
    
    Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

---
+++ arch/arm/mach-k3/am625_fdt.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright 2023 Toradex - https://www.toradex.com/
+ */
+
+#include <fdt_support.h>
+
+int ft_system_setup(void *blob, struct bd_info *bd)
+{
+	return 0;
+}
